069c1dfb84afcc642ce76e6fbe287f2942356621
Anna <aik303030@gmail.com>
2021-03-08 22:49:40 +0300

    8-module 2-task

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,46 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+
+    this.elem = document.createElement('div');
+    this.elem.insertAdjacentHTML('afterbegin', `
+      <div class="products-grid">
+        <div class="products-grid__inner">
+        </div>
+      </div>
+    `);
+
+    this.elem.classList.add('.products-grid');
+
+    this.productsInner = this.elem.querySelector('.products-grid__inner');
+
+    this.products.forEach(product => {
+      const productCard = new ProductCard(product);
+      this.productsInner.append(productCard.elem);
+    });
+  }
+
+  updateFilter(filters) {
+    this.filters = Object.assign(this.filters, filters);
+
+    let filteredProducts = this.products.filter(product => {
+      if (this.filters.noNuts === true && product.nuts === true) {
+        return false;
+      }
+      if (this.filters.vegeterianOnly === true && product.vegeterian !== true) {
+        return false;
+      }
+      if (this.filters.maxSpiciness && this.filters.maxSpiciness < product.spiciness) {
+        return false;
+      }
+      return !(this.filters.category && this.filters.category !== product.category);
+    });
+
+    this.productsInner.innerHTML = '';
+
+    filteredProducts.forEach(product => {
+      const productCard = new ProductCard(product);
+      this.productsInner.append(productCard.elem);
+    });
   }
 }
